e613a866bc739b1eea1c86d31101a5afecb8db59
liandraluiza <liandraluiza.02@gmail.com>
2025-06-01 12:17:59 -0300

    Iniciando o curso de JavaScript

---
+++ aula04/comentarios.js
@@ -0,0 +1,9 @@
+// Use as barras para comentar linhas
+// Use /**/ para comentar blocos de código
+
+console.log('Olá Mundo!')
+console.log('linha 1')
+/*console.log('linha 2') 
+console.log('linha 3')    
+console.log('linha 4')    
+console.log('linha 5')*/       
