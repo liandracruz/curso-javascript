085d9b390b6cf07c684f18985a5977764f54190b
liandracruz <liandraluiza.02@gmail.com>
2025-07-07 17:57:36 -0300

    exercitando javaScript

--- aula18/assets/js/script.js
+++ aula18/assets/js/script.js
@@ -2,11 +2,18 @@ const form = document.querySelector('#form')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
+    console.log('Evento prevenido')
     setResultado('Olá Mundo!')
 })
+
+function criaP(className) {
+    const p = document.createElement('p')
+    p.classList.add(className)
+    return 
+}
 
 function setResultado(msg) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = '';
-    con
+    resultado.appendChild(p)
 }
