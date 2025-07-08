e55b26ac3b5ffd0879b8f1c91af70db3e97548ca
liandracruz <liandraluiza.02@gmail.com>
2025-07-08 05:33:56 -0300

    praticando

--- aula18/assets/js/script.js
+++ aula18/assets/js/script.js
@@ -2,18 +2,41 @@ const form = document.querySelector('#form')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
-    console.log('Evento prevenido')
-    setResultado('Olá Mundo!')
+    const inputPeso = event.target.querySelector('#peso')
+    const inputAltura = event.target.querySelector('#altura')
+
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if(!peso) {
+        setResultado('Peso inválido', false)
+        return
+    }
+
+    if(!altura) {
+        setResultado('Altura inválida', false)
+        return
+    }
+
+    const imc = getImc(peso, altura)
 })
 
-function criaP(className) {
-    const p = document.createElement('p')
-    p.classList.add(className)
-    return 
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+    setResultado(`Seu IMC é ${imc}`, true)
 }
 
-function setResultado(msg) {
+function criaP() {
+    const p = document.createElement('p')
+    return p
+}
+
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = '';
+
+    const p = criaP()
+    p.innerHTML= msg
     resultado.appendChild(p)
 }
