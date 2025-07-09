109557d968eeb32b78865846582d87f5ecddf17d
liandracruz <liandraluiza.02@gmail.com>
2025-07-09 16:57:40 -0300

    criando uma calculadora de imc

--- aula18/assets/js/script.js
+++ aula18/assets/js/script.js
@@ -1,9 +1,9 @@
 const form = document.querySelector('#form')
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const inputPeso = event.target.querySelector('#peso')
-    const inputAltura = event.target.querySelector('#altura')
+form.addEventListener('submit', function(evento) {
+    evento.preventDefault()
+    const inputPeso = evento.target.querySelector('#peso')
+    const inputAltura = evento.target.querySelector('#altura')
 
     const peso = Number(inputPeso.value)
     const altura = Number(inputAltura.value)
@@ -11,7 +11,7 @@ form.addEventListener('submit', function(event) {
     if(!peso) {
         setResultado('Peso inválido', false)
         return
-    }
+    } 
 
     if(!altura) {
         setResultado('Altura inválida', false)
@@ -19,12 +19,27 @@ form.addEventListener('submit', function(event) {
     }
 
     const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`
+
+    setResultado(msg, true)
 })
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if(imc >= 39.9) return nivel[5]
+    if(imc >= 34.9) return nivel[4]
+    if(imc >= 29.9) return nivel [3]
+    if(imc >= 24.9) return nivel [2]
+    if(imc >= 18.5) return nivel[1]
+    if(imc < 18.5) return nivel[0]
+}
 
 function getImc(peso, altura) {
     const imc = peso / altura ** 2
     return imc.toFixed(2)
-    setResultado(`Seu IMC é ${imc}`, true)
 }
 
 function criaP() {
@@ -34,9 +49,15 @@ function criaP() {
 
 function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado')
-    resultado.innerHTML = '';
+    resultado.innerHTML = '' 
 
     const p = criaP()
-    p.innerHTML= msg
+
+    if(isValid) {
+        p.classList.add('paragrafo-resultado')
+    } else {
+        p.classList.add('bad')
+    } 
+    p.innerHTML = msg
     resultado.appendChild(p)
 }
